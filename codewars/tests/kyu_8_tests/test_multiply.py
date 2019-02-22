2ffa451a684b0161a456bfc77f924e7a3726603d
Elias Neves <elias-neves93@hotmail.com>
2019-02-22 17:16:57 -0300

    Added a test multiply

--- codewars/tests/kyu_8_tests/test_multiply.py
+++ codewars/tests/kyu_8_tests/test_multiply.py
@@ -5,6 +5,7 @@ from codewars.katas.kyu_8.multiply import multiply
 class Teste_multiply(TestCase):
     def test_multiply(self):
         self.assertEqual(multiply(2,2), 4)
+        self.assertEqual(multiply(5,2), 10)
 
 if __name__ == '__main__':
     main()
