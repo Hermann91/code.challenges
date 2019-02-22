e0a284799e439c6af0a90952b0d387841bea0c54
Elias Neves <elias-neves93@hotmail.com>
2019-02-22 09:35:21 -0300

    kyu_8 - multiply solved

---
+++ codewars/tests/kyu_8_tests/test_multiply.py
@@ -0,0 +1,10 @@
+from unittest import TestCase, main
+
+from katas.kyu_8.multiply import multiply
+
+class Teste_multiply(TestCase):
+    def test_multiply(self):
+        self.assertEqual(multiply(2,2), 4)
+
+if __name__ == '__main__':
+    main()
